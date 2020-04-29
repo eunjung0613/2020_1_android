ca3490e728219e2dde793428a55bd6581215076f
dmswjd7281 <dmswjd7281@gmail.com>
2020-04-29 23:29:44 +0900

    E03List / RecyclerView3

--- E03List/app/src/main/java/net/skhu/Memo.java
+++ E03List/app/src/main/java/net/skhu/Memo.java
@@ -2,18 +2,26 @@ package net.skhu;
 
 import android.provider.ContactsContract;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Memo {
+public class Memo implements Serializable {
     final static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     String title;
+    String body;
     Date date;
     boolean checked;
 
     public Memo(String title, Date date){
         this.title = title;
         this.date = date;
+    }
+    public Memo(String title, String body, Date date){
+        this.title = title;
+        this.body = body;
+        this.date = date;
+
     }
     public String getTitle(){
         return title;
@@ -30,9 +38,9 @@ public class Memo {
     public String getDateFormatted(){
         return format.format(date);
     }
-    public boolean isChecked(){
-        return checked;
-    }
+    public String getBody() { return body; }
+    public void setBody(String body) { this.body = body; }
+    public boolean isChecked(){ return checked; }
     public void setChecked(boolean checked){
         this.checked = checked;
     }
