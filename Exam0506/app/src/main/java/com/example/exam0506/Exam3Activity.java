1d09c88dfa07a479c7c49f7943a821cf4380c457
eunjung0613 <37800677+eunjung0613@users.noreply.github.com>
2020-05-06 17:30:58 +0900

    201814003-3
    201814003-3

--- Exam0506/app/src/main/java/com/example/exam0506/Exam3Activity.java
+++ Exam0506/app/src/main/java/com/example/exam0506/Exam3Activity.java
@@ -1,14 +1,49 @@
 package com.example.exam0506;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class Exam3Activity extends AppCompatActivity {
+
+    Exam3Adapter Exam3Adapter;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam3);
+
+        arrayList = new ArrayList<String>();
+        arrayList.add("one");
+        arrayList.add("two");
+
+        Exam3Adapter = new Exam3Adapter(this,arrayList);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_exam3, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.btn_create) {
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
