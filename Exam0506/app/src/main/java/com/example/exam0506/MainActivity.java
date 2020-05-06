1c65f8d29d3cce5faa56b3365262948709627a13
eunjung0613 <37800677+eunjung0613@users.noreply.github.com>
2020-05-06 17:10:49 +0900

    201814003-2
    201814003-2

--- Exam0506/app/src/main/java/com/example/exam0506/MainActivity.java
+++ Exam0506/app/src/main/java/com/example/exam0506/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+    }
+    public void btn_clicked(View view){
+        Intent intent = new Intent(this,Exam3Activity.class);
+        startActivity(intent);
     }
 }
